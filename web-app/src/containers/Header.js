071c214e16cf06b100cdc5e5582c3626f3164402
chung-shih-jou <dirtywolf2009@gmail.com>
2020-09-23 09:56:10 +0800

    menu with title button

--- web-app/src/containers/Header.js
+++ web-app/src/containers/Header.js
@@ -4,12 +4,12 @@ import { Navbar, Nav, NavDropdown, Form, FormControl, Button} from 'react-bootst
 function Header() {
     return (
         <Navbar bg="dark" expand="lg">
-            <Navbar.Brand href="#home">XX餐廳 <small>by FoodieSplit</small></Navbar.Brand>
+            <Navbar.Brand href="/#">泰媽媽餐廳 <small>by FoodieSplit</small></Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#link">Link</Nav.Link>
+                    <Nav.Link href="/#menu">菜單</Nav.Link>
+                    <Nav.Link href="link">Link</Nav.Link>
                     <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                         <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
--- web-app/src/containers/Menu.js
+++ web-app/src/containers/Menu.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { Component } from "react";
+import { Row, Button } from "react-bootstrap";
+import Mydb from "./mydb.json";
 
-function Menu() {
+const menu = Mydb.menu;
+export default class Menu extends Component {
+  state = { category: "" };
+  handleCategorySelect = category => {
+      console.log(category.target)
+    this.setState({ category });
+  };
+
+  render() {
     return (
-        <>
-            Menu
-        </>
-    )
+      <>
+        <Row
+          style={{
+            background: "black",
+            justifyContent: "center",
+            padding: "10px"
+          }}
+        >
+          <div>
+            {menu.categories.map(category => (
+              <Button
+                style={{ marginRight: "5px" }}
+                key={category.id}
+                variant="light"
+                onClick={(e)=>this.handleCategorySelect(e.target.value)}
+                value={category.name}
+              >
+                {category.name}
+              </Button>
+            ))}
+          </div>
+        </Row>
+        <span>{this.state.category}</span>
+        {menu.products.map(product => (
+          <Row></Row>
+        ))}
+      </>
+    );
+  }
 }
-
-export default Menu;
