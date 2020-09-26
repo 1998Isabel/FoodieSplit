31b73be087c63e45c7f6f5d456271d2bee8145e0
I CHIEN LAI <isabel@Is-MBP-2.Dlink>
2020-09-26 18:45:02 +0800

    add demo_pay for payment

---
+++ demo_pay/build/precache-manifest.cb5f165c780e6b0555d14ecdeb5ec3bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6612b486bf39b261fffb7fd63ee4f2ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0b7c330efbf46ae4684",
+    "url": "/static/css/2.0012cb62.chunk.css"
+  },
+  {
+    "revision": "0434ff2539825a834f00",
+    "url": "/static/css/main.7784031e.chunk.css"
+  },
+  {
+    "revision": "e0b7c330efbf46ae4684",
+    "url": "/static/js/2.d4880293.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.d4880293.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0434ff2539825a834f00",
+    "url": "/static/js/main.889255d0.chunk.js"
+  },
+  {
+    "revision": "c4cda4214c0c300269f1",
+    "url": "/static/js/runtime-main.d5bfd598.js"
+  }
+]);
